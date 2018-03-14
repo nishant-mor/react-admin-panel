2248592219806ed4e14e5cd13c45532019ba24f9
Nishant Mor <nishantmor27@gmail.com>
2018-03-14 16:24:57 +0530

    Initial app structure, basic routing

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+// This Component handles the app template used on every page.
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div className="container-fluid">
+      <Header/>
+      {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  childern : PropTypes.object.isRequired
+};
+
+export default App;
